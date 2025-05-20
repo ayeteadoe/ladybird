fc5d15fabdccc56e64643af57d0c2bd4ccfe17aa
Ashton <ayeteadoe@gmail.com>
2025-05-20 05:31:34 -0700

    Meta: Add rebuild command to ladybird.py

--- Meta/ladybird.py
+++ Meta/ladybird.py
@@ -114,6 +114,12 @@ def main(platform):
 
     subparsers.add_parser('clean', help='Cleans the build environment', parents=[preset_parser, compiler_parser])
 
+    rebuild_parser = subparsers.add_parser('rebuild',
+                                           help='Cleans the build environment and compiles the target binaries',
+                                           parents=[preset_parser, compiler_parser, target_parser])
+    rebuild_parser.add_argument('args', nargs=argparse.REMAINDER,
+                                help='Additional arguments passed through to the build system')
+
     args = parser.parse_args()
     kwargs = vars(args)
     command = kwargs.pop('command', None)
@@ -165,6 +171,10 @@ def main(platform):
         _build_vcpkg()
     elif command == 'clean':
         _clean_main(**kwargs)
+    elif command == 'rebuild':
+        _clean_main(**kwargs)
+        build_dir = _configure_main(platform, **kwargs)
+        _build_main(build_dir, **kwargs)
 
 
 def _configure_main(platform, **kwargs):
