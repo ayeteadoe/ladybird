952334617645e9b523b4cb8ffc88a50e52d96257
ayeteadoe <ayeteadoe@gmail.com>
2025-11-18 21:05:34 -0800

    LibWeb/WebGPU: Implement GPUTextureUsage IDL

---
+++ Libraries/LibWeb/WebGPU/GPUTextureUsage.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2025, ayeteadoe <ayeteadoe@gmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+// NOTE: This header is intentionally empty as GPUTextureUsage is a namespace that only contains constant fields;
+//       however, our IDL generator infrastructure assumes we need to include a header file in the generated
+//       implementation.
